6ac97fcd497505b306b49f35cff47a3a4d19735d
Georgi <ggeorgi60@gmail.com>
2024-02-02 12:51:52 +0200

    Initial commit with somewhat of a usable config

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+pkill polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 
+echo "---" | tee -a /tmp/polybar1.log 
+polybar mainbar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+## Launch bar_bottom
+#echo "---" | tee -a /tmp/polybar2.log 
+#polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
