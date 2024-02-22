4c38330773df3ddfeb531a006af58d5d4c92781e
Georgi Nikolov <ggeorgi60@gmail.com>
2024-02-22 13:49:55 +0200

    Added automatic garbage collection in nix every week

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,11 @@
     allowUnfree = true;
   };
 
+  nix.gc = {
+    automatic = true;  # Enable the automatic garbage collector
+    dates = "weekly";   # When to run the garbage collector
+    options = "-d";    # Arguments to pass to nix-collect-garbage
+  };
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
