66e4c500ee1007cc3471640de8b3fcc1f18a0c92
Georgi Nikolov <ggeorgi60@gmail.com>
2024-04-01 19:00:54 +0300

    Added a default home-manager configuration

--- flake.nix
+++ flake.nix
@@ -4,18 +4,34 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ...}:
+  outputs = {self, nixpkgs, home-manager, ...}:
   let
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
+
     nixosConfigurations = {
+
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ./machines/vivobook/hardware-configuration.nix ./temp.nix ];
       };
-    };
-  };
 
+    };
+
+    homeConfigurations = {
+
+      AzSamSi = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+
+    };
+
+  };
 }
