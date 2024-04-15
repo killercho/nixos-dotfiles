43b3331232d9cd0d57fac3b0d7621c0cca82faee
Georgi Nikolov <ggeorgi60@gmail.com>
2024-04-15 18:24:39 +0300

    Added a temp module for wayland

--- wayland/system/configuration.nix
+++ wayland/system/configuration.nix
@@ -10,6 +10,7 @@
       ./../../machines/vivobook/hardware-configuration.nix # Include the results of the hardware scan.
       ./hyprland.nix
       ./stylix.nix
+      ./temp.nix
     ];
 
   # Use the GRUB 2 boot loader.
---
+++ wayland/system/temp.nix
@@ -0,0 +1,12 @@
+# This is a module configuring all the things that are in the configuration temporarily
+{ config, pkgs, ... }:
+
+{
+  # MySQL for the Databases course
+  services.mysql.enable = true;
+  services.mysql.package = pkgs.mysql;
+
+  environment.systemPackages = with pkgs; [
+    R
+  ];
+}
