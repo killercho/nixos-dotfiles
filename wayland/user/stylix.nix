0d748c3d28d10e3f6e5f34aa991a36c6b840a18e
Georgi Nikolov <ggeorgi60@gmail.com>
2024-04-18 15:36:25 +0300

    Removed swaylock module

--- wayland/user/stylix.nix
+++ wayland/user/stylix.nix
@@ -5,9 +5,15 @@
   stylix = {
     image = /usr/share/backgrounds/background.jpg;
     polarity = "dark";
+
+    cursor = {
+      size = 16;
+    };
+
     opacity = {
       applications = 0.8;
       terminal = 0.7;
     };
+
   };
 }
