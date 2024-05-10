4fa3e6c8ba6625f08a72f0c875385715261cd7db
Georgi Nikolov <ggeorgi60@gmail.com>
2024-05-10 17:30:21 +0300

    Changed waybar bubbles to be transparent with a border

--- wayland/user/waybar-style.nix
+++ wayland/user/waybar-style.nix
@@ -33,39 +33,40 @@
                 }
 
       #workspaces {
-                  background-color: #'' + config.lib.stylix.colors.base00 + '';
-                    color: #'' + config.lib.stylix.colors.base02 + '';
+                  /* background-color: #'' + config.lib.stylix.colors.base00 + ''; */
+                    background-color: rgba(0, 0, 0, 0.5);
                       border-radius: 16px;
-                      }
-
-      #workspaces button:hover {
-                      background-color: #'' + config.lib.stylix.colors.base07 + '';
-                        color: #000000;
-                          border-radius: 16px;
+                      border: 2px solid #'' + config.lib.stylix.colors.base00 + '';
                         }
 
-      #workspaces button.active {
-                        background-color: #'' + config.lib.stylix.colors.base08 + '';
-                          color: #000000;
-                          border-radius: 16px;
-                          }
-
-      #workspaces button.focused {
-                          background-color: #'' + config.lib.stylix.colors.base0A + '';
-                            color: #000000;
-                            }
-
-      #workspaces button.visible {
-                            background-color: #'' + config.lib.stylix.colors.base05 + '';
-                              color: #000000;
-                              border-radius: 16px;
+      #workspaces button:hover {
+                          background-color: rgba(0, 0, 0, 0.5);
+                            border-radius: 16px;
+                            border: none;
                               }
 
+      #workspaces button.active {
+                                background-color: rgba(0, 0, 0, 0.5);
+                                  border-radius: 16px;
+                                  border: none;
+                                    }
+
+      #workspaces button.focused {
+                                    background-color: #'' + config.lib.stylix.colors.base0A + '';
+                                      color: #000000;
+                                      }
+
+      #workspaces button.visible {
+                                      background-color: #'' + config.lib.stylix.colors.base05 + '';
+                                        color: #000000;
+                                        border-radius: 16px;
+                                        }
+
       #workspaces button.urgent {
-                              background-color: #'' + config.lib.stylix.colors.base09 + '';
-                                color: #ffffff;
-                                border-radius: 16px;
-                                }
+                                          background-color: rgba(0, 0, 0, 0.5);
+                                          border: 1px solid #'' + config.lib.stylix.colors.base09 + '';
+                                            border-radius: 16px;
+                                            }
 
       #clock,
       #language,
@@ -84,106 +85,119 @@
       #idle_inhibitor,
       #scratchpad,
       #mpd {
-                                padding: 0 10px;
-                                background-color: #'' + config.lib.stylix.colors.base07 + '';
-                                  color: #000000;
-                                  border: 0px;
-                                  border-radius: 16px;
-                                  }
+                                            padding: 0 10px;
+                                            background-color: rgba(0, 0, 0, 0.5);
+                                            color: #000000;
+                                            border: 2px solid #'' + config.lib.stylix.colors.base07 + '';
+                                              border-radius: 16px;
+                                              }
 
       #window,
       #workspaces {
-                                  margin: 0 4px;
-                                  }
+                                              margin: 0 4px;
+                                              }
 
-                                  /* If workspaces is the leftmost module, omit left margin */
-                                  .modules-left > widget:first-child > #workspaces {
-                                  margin-left: 0;
-                                  }
+                                              /* If workspaces is the leftmost module, omit left margin */
+                                              .modules-left > widget:first-child > #workspaces {
+                                              margin-left: 0;
+                                              }
 
-                                  /* If workspaces is the rightmost module, omit right margin */
-                                  .modules-right > widget:last-child > #workspaces {
-                                  margin-right: 0;
-                                  }
+                                              /* If workspaces is the rightmost module, omit right margin */
+                                              .modules-right > widget:last-child > #workspaces {
+                                              margin-right: 0;
+                                              }
       #language {
-                                  color: #'' + config.lib.stylix.colors.base07 + '';
-                                    background-color: #'' + config.lib.stylix.colors.base02 + '';
-                                      }
-
-      #clock {
-                                      color: #000000;
-                                      background-color: #'' + config.lib.stylix.colors.base0D + '';
-                                        }
-
-      #battery {
-                                        color: #000000;
-                                        background-color: #'' + config.lib.stylix.colors.base0B + '';
-                                          }
-
-      #battery.charging, #battery.plugged {
-                                          color: #ffffff;
-                                          background-color: #'' + config.lib.stylix.colors.base0C + '';
-                                            }
-
-                                            @keyframes blink {
-                                            to {
-                                            background-color: #'' + config.lib.stylix.colors.base07 + '';
-                                              color: #ffffff;
-                                              }
-                                              }
-
-      #battery.critical:not(.charging) {
-                                              background-color: #'' + config.lib.stylix.colors.base08 + '';
+                                              /* color: #'' + config.lib.stylix.colors.base02 + ''; */
                                                 color: #ffffff;
-                                                animation-name: blink;
-                                                animation-duration: 0.5s;
-                                                animation-timing-function: linear;
-                                                animation-iteration-count: infinite;
-                                                animation-direction: alternate;
-                                                }
-
-                                                label:focus {
-                                                background-color: #'' + config.lib.stylix.colors.base00 + '';
+                                                border: 2px solid #'' + config.lib.stylix.colors.base02 + '';
+                                                  background-color: rgba(0, 0, 0, 0.5);
                                                   }
 
-      #cpu {
-                                                  background-color: #'' + config.lib.stylix.colors.base0D + '';
-                                                    color: #000000;
+      #clock {
+                                                  color: #ffffff;
+                                                  border: 2px solid #'' + config.lib.stylix.colors.base0D + '';
+                                                    background-color: rgba(0, 0, 0, 0.5);
                                                     }
 
-      #memory {
-                                                    background-color: #'' + config.lib.stylix.colors.base0E + '';
-                                                      color: #000000;
+      #battery {
+                                                    color: #ffffff;
+                                                    border: 2px solid #'' + config.lib.stylix.colors.base0B + '';
+                                                      background-color: rgba(0, 0, 0, 0.5);
                                                       }
 
-      #disk {
-                                                      background-color: #'' + config.lib.stylix.colors.base0F + '';
-                                                        color: #000000;
+      #battery.charging, #battery.plugged {
+                                                      color: #ffffff;
+                                                      border: 2px solid #'' + config.lib.stylix.colors.base0C + '';
+                                                        background-color: rgba(0, 0, 0, 0.5);
                                                         }
 
-      #backlight {
-                                                        background-color: #'' + config.lib.stylix.colors.base0A + '';
-                                                          color: #000000;
+                                                        @keyframes blink {
+                                                        to {
+                                                        color: #ffffff;
+                                                        border: 2px solid #'' + config.lib.stylix.colors.base07 + '';
+                                                          background-color: rgba(0, 0, 0, 0.5);
+                                                          }
                                                           }
 
-      #pulseaudio {
-                                                          background-color: #'' + config.lib.stylix.colors.base0C + '';
-                                                            color: #000000;
+      #battery.critical:not(.charging) {
+                                                          color: #ffffff;
+                                                          border: 2px solid #'' + config.lib.stylix.colors.base08 + '';
+                                                            background-color: rgba(0, 0, 0, 0.5);
+                                                            animation-name: blink;
+                                                            animation-duration: 0.5s;
+                                                            animation-timing-function: linear;
+                                                            animation-iteration-count: infinite;
+                                                            animation-direction: alternate;
+                                                            }
+
+                                                            label:focus {
+                                                            background-color: #'' + config.lib.stylix.colors.base00 + '';
+                                                              }
+
+      #cpu {
+                                                              color: #ffffff;
+                                                              border: 2px solid #'' + config.lib.stylix.colors.base0D + '';
+                                                                background-color: rgba(0, 0, 0, 0.5);
                                                                 }
 
-      #pulseaudio.muted {
-                                                            background-color: #'' + config.lib.stylix.colors.base04 + '';
-                                                              color: #000000;
+      #memory {
+                                                                color: #ffffff;
+                                                                border: 2px solid #'' + config.lib.stylix.colors.base0E + '';
+                                                                  background-color: rgba(0, 0, 0, 0.5);
                                                                   }
+
+      #disk {
+                                                                  color: #ffffff;
+                                                                  border: 2px solid #'' + config.lib.stylix.colors.base0F + '';
+                                                                    background-color: rgba(0, 0, 0, 0.5);
+                                                                    }
+
+      #backlight {
+                                                                    color: #ffffff;
+                                                                    border: 2px solid #'' + config.lib.stylix.colors.base0A + '';
+                                                                      background-color: rgba(0, 0, 0, 0.5);
+                                                                      }
+
+      #pulseaudio {
+                                                                      color: #ffffff;
+                                                                      border: 2px solid #'' + config.lib.stylix.colors.base0C + '';
+                                                                        background-color: rgba(0, 0, 0, 0.5);
+                                                                        }
+
+      #pulseaudio.muted {
+                                                                        color: #ffffff;
+                                                                        border: 2px solid #'' + config.lib.stylix.colors.base04 + '';
+                                                                          background-color: rgba(0, 0, 0, 0.5);
+                                                                          }
 
       #tray > .passive {
-                                                                  -gtk-icon-effect: dim;
-                                                                  }
+                                                                          -gtk-icon-effect: dim;
+                                                                          }
 
       #tray > .needs-attention {
-                                                                  -gtk-icon-effect: highlight;
-                                                                  }
+                                                                          -gtk-icon-effect: highlight;
+                                                                          }
 
-                                                            '';
-                                                          };
-                                                        }
+                                                                        '';
+                                                                      };
+                                                                    }
