79d82bd1db98358c75a43f85cdca90a7bd04876f
Georgi Nikolov <ggeorgi60@gmail.com>
2024-06-05 17:59:26 +0300

    Adjusted dunst timeouts

--- modules/notifications.nix
+++ modules/notifications.nix
@@ -15,7 +15,7 @@
         offset = "10x20";
         origin = "top-right";
         transparency = 10;
-        timeout = lib.mkDefault 5;
+        timeout = lib.mkDefault 3;
         frame_color = "#eceff1";
         font = lib.mkDefault "Droid Sans 9";
       };
@@ -23,7 +23,7 @@
       urgency_normal = {
         background = lib.mkDefault "#37474f";
         foreground = lib.mkDefault "#eceff1";
-        timeout = lib.mkDefault 5;
+        timeout = lib.mkDefault 4;
       };
 
     };
