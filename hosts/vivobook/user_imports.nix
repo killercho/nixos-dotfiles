95b86cc802cb3553ce90385e40cef26470200b8c
Georgi Nikolov <ggeorgi60@gmail.com>
2024-06-25 16:39:44 +0300

    Moved the imports into hosts specific location

---
+++ hosts/vivobook/user_imports.nix
@@ -0,0 +1,47 @@
+{ ... }:
+
+{
+  imports = [
+    # Home.nix split up into two files
+    ./../../modules/user/home_settings.nix
+    ./../../modules/user/user_packages.nix
+
+    # User shell configuration
+    ./../../modules/user/zsh.nix
+
+    # User git configuration
+    ./../../modules/user/git.nix
+
+    # User terminal configuration
+    ./../../modules/user/alacritty.nix
+
+    # User app launcher configuration
+    ./../../modules/user/fuzzel.nix
+
+    # User neovim configuration
+    ./../../modules/user/neovim/neovim.nix
+
+    # Development needed tools
+    ./../../modules/user/dev.nix
+
+    # User stylix configuration
+    ./../../modules/user/stylix.nix
+
+    # User hyprland configuration
+    ./../../modules/user/hyprland/hyprland.nix
+
+    # Notification popups configuration
+    ./../../modules/user/notifications.nix
+
+    # User waybar configuration
+    ./../../modules/user/waybar/waybar.nix
+    ./../../modules/user/waybar/waybar-modules.nix
+    ./../../modules/user/waybar/waybar-style.nix
+
+    # Volume popup script
+    ./../../scripts/volume.nix
+
+    # Battery low warning
+    ./../../scripts/battery_notifier.nix
+  ];
+}
