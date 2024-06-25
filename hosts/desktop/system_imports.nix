59bed2d833ed5cd89e7924157e671bd6e87cbc51
Georgi Nikolov <ggeorgi60@gmail.com>
2024-06-25 16:41:26 +0300

    Added a desktop specific host default configuration

---
+++ hosts/desktop/system_imports.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  imports = [
+    # Hardware configuration for the laptop
+    ./../../machines/vivobook/hardware-configuration.nix
+
+    # Configuration.nix split into two files
+    ./../../modules/system/system_settings.nix
+    ./../../modules/system/system_packages.nix
+
+    # Hyperland enable and default setup
+    ./../../modules/system/hyprland.nix
+
+    # Stylix enable and global settings
+    ./../../modules/system/stylix.nix
+
+    # Global temporary packages and settings
+    ./../../modules/system/temp.nix
+  ];
+}
