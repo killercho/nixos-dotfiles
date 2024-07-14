49ac076673bda92bafd0b3fbea5e2f785e18c819
Georgi Nikolov <ggeorgi60@gmail.com>
2024-07-14 11:25:01 +0300

    Added mongohud configuration for the desktop

--- modules/user/desktop_specific.nix
+++ modules/user/desktop_specific.nix
@@ -3,5 +3,13 @@
   home.packages = with pkgs; [
   ];
 
-  programs.mangohud.enable = true;
+  programs.mangohud = {
+    enable = true;
+    settings = {
+      cpu_temp = true;
+      cpu_mhz = true;
+      gpu_temp = true;
+      ram = true;
+    };
+  };
 }
