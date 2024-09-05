0b32a7d5a67aa18f6b007d5b1a2aaf625ba10e4f
Georgi Nikolov <georgi.s.nikolov@egt.com>
2024-09-05 09:20:50 +0300

    Added a clangd configuration

---
+++ modules/user/neovim/clangd.nix
@@ -0,0 +1,12 @@
+# Clangd configuration for my work machine
+{ config, pkgs, ... }:
+
+{
+  home.file.".config/clangd/config.yaml".text = ''
+    CompileFlags:
+      Add: [
+        "-I/usr/include/c++/9",
+        "-I/usr/include/x86_64-linux-gnu/c++/9",
+      ]
+  '';
+}
