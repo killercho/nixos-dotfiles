815c1eef11d25d97b9efe958e5ce397b7f2dc649
Georgi Nikolov <ggeorgi60@gmail.com>
2025-01-11 11:35:10 +0200

    Added php to the temp

--- modules/system/temp.nix
+++ modules/system/temp.nix
@@ -3,5 +3,24 @@
 
 {
   environment.systemPackages = with pkgs; [
+    apacheHttpd
   ];
+
+  services.httpd = {
+    enable = true;
+    enablePHP = true;
+    user = "AzSamSi";
+
+    virtualHosts = {
+      web-project = {
+        hostName = "web-project.local";
+        listen = [{ port = 80; }];
+        documentRoot = "/home/AzSamSi/Programming/Web/nowayforward_human/";
+      };
+    };
+  };
+
+  networking.extraHosts = ''
+    127.0.0.1 web-project.local
+  '';
 }
