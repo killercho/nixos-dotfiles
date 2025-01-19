c859c564f08a4036c8bdba729a6ba5de3546ea0b
Georgi Nikolov <ggeorgi60@gmail.com>
2025-01-19 11:52:18 +0200

    Removed apache and mysql

--- modules/system/temp.nix
+++ modules/system/temp.nix
@@ -3,29 +3,5 @@
 
 {
   environment.systemPackages = with pkgs; [
-    apacheHttpd
   ];
-
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-  };
-
-  services.httpd = {
-    enable = true;
-    enablePHP = true;
-    user = "AzSamSi";
-
-    virtualHosts = {
-      web-project = {
-        hostName = "web-project.local";
-        listen = [{ port = 80; }];
-        documentRoot = "/home/AzSamSi/Programming/Web/nowayforward_human/";
-      };
-    };
-  };
-
-  networking.extraHosts = ''
-    127.0.0.1 web-project.local
-  '';
 }
