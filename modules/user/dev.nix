3bd2d138db46f84c9308158c14e1ed8e8f366bd3
Georgi Nikolov <georgi.s.nikolov@egt.com>
2025-03-14 13:10:03 +0200

    Added ranger to the dev packages

--- modules/user/dev.nix
+++ modules/user/dev.nix
@@ -18,4 +18,26 @@
   ];
 
   programs.ripgrep.enable = true;
+
+  programs.ranger = {
+    enable = true;
+    rifle = [ ];
+    mappings = {
+      f = "console fzf_filter%space";
+    };
+    aliases = { };
+    plugins = [
+      {
+        name = "ranger-fzf-filter";
+        src = builtins.fetchGit { 
+          url = "https://github.com/MuXiu1997/ranger-fzf-filter.git";
+          rev = "bf16de2e4ace415b685ff7c58306d0c5146f9f43";
+        };
+      }
+    ];
+    settings = {
+      column_ratios = "2,3,2";
+    };
+    extraConfig = "";
+  };
 }
