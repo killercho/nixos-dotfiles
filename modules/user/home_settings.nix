dc52b4e08f59454a1a703b3bb501977319e9a3bf
Georgi Nikolov <ggeorgi60@gmail.com>
2025-04-03 20:30:36 +0300

    Added a configuration for the auto expire service that is still not released

--- modules/user/home_settings.nix
+++ modules/user/home_settings.nix
@@ -20,7 +20,20 @@
     allowUnfree = true;
   };
 
-  services.udiskie.enable = true;
+  # TODO:
+  # This feature is still disabled
+  # Enable it at some point when it comes
+  #services = {
+  #udiskie.enable = true;
+  #autoExpire =
+  #{
+  #enable = true;
+  #timestamp = "-7 days";
+  #frequency = "weekly";
+  #store.cleanup = true;
+  #store.options = "--delete-older-than 30d";
+  #};
+  #};
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -30,16 +43,16 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   #home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+  # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+  # # symlink to the Nix store copy.
+  # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  # # You can also set the file content immediately.
+  # ".gradle/gradle.properties".text = ''
+  #   org.gradle.console=verbose
+  #   org.gradle.daemon.idletimeout=3600000
+  # '';
   #};
 
   # Home Manager can also manage your environment variables through
