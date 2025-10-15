2512979c25cf3cb34b87bb989d9c6b310f88c0a2
Georgi Nikolov <ggeorgi60@gmail.com>
2025-10-15 20:00:25 +0300

    Fixed the hyprland gestures being depicated

--- modules/user/hyprland/hyprland.nix
+++ modules/user/hyprland/hyprland.nix
@@ -126,10 +126,7 @@
       }
 
       # Enable touchpad gestures
-      gestures {
-        workspace_swipe = true
-        workspace_swipe_fingers = 3
-      }
+      gesture = 3, horizontal, workspace
 
       misc {
       # See https://wiki.hyprland.org/Configuring/Variables/ for more
