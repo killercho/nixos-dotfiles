d06a6cb52ce95018a00b07246f8f65ce14d4f24c
Georgi Nikolov <ggeorgi60@gmail.com>
2026-03-29 16:25:18 +0300

    Disabled tlp messing with the wake on lan options

--- modules/system/system_settings.nix
+++ modules/system/system_settings.nix
@@ -112,6 +112,7 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+      WOL_DISABLE = "N";
     };
   };
 
