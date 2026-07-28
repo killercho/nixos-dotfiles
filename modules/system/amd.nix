89b03dbb22a13a7ae227acb27b800a09c9d7686c
Georgi Nikolov <ggeorgi60@gmail.com>
2026-07-28 19:19:04 +0300

    Updated system

--- modules/system/amd.nix
+++ modules/system/amd.nix
@@ -6,10 +6,11 @@
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   environment.systemPackages = with pkgs; [
+    #libdisplay-info
     microcode-amd
-    lact
+    #lact
   ];
 
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+  #systemd.packages = with pkgs; [ lact ];
+  #systemd.services.lactd.wantedBy = [ "multi-user.target" ];
 }
